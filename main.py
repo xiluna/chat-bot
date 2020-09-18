b52f2b769305a282532d8f80b29fd7f39b764c75
xiluna <xilaluna2@gmail.com>
2020-09-17 18:25:27 -0700

    upgrade 0.1

--- main.py
+++ main.py
@@ -1,34 +1,42 @@
 from random import choice
+
 exit_command = 'exit'
 
 
 def get_weather_bot_response(user):
-    bot_response_sunny = ['Wear some sunglasses. ',
-                          'Going to the pool would be nice today. ']
-    bot_response_rainy = ['Better get an umbrella. ',
-                          'You should stay inside. ', 'Dont get cold you might get sick. ']
-    bot_response_smokey = ['California is on FIRE!! ', 'The air qulaity is horrible wear a mask. ',
-                           'We need to fund ideas to stop climate change and stop big oil companies. ']
-    bot_response_foggy = ['Kinda of chilly, better wear a light jacket. ',
-                          'Be careful when driving. ', 'It might rain. ']
-    bot_response_snowy = ['It would be nice to snowboard or ski. ',
-                          'Snowball fights are always fun. ', 'Hot coco on snowy days is nice. ']
+    bot_responses = {
+        'sunny': ['Wear some sunglasses. ',
+                  'Going to the pool would be nice today. '],
+
+        'rainy': ['Better get an umbrella. ',
+                  'You should stay inside. ', 'Dont get cold you might get sick. '],
+
+        'smokey': ['California is on FIRE!! ', 'The air qulaity is horrible wear a mask. ',
+                   'We need to fund ideas to stop climate change and stop big oil companies. '],
+
+        'foggy': ['Kinda of chilly, better wear a light jacket. ',
+                  'Be careful when driving. ', 'It might rain. '],
+
+        'snowy': ['It would be nice to snowboard or ski. ',
+                  'Snowball fights are always fun. ', 'Hot coco on snowy days is nice. ']
+    }
 
     if user == 'sunny':
-        return choice(bot_response_sunny)
+        return choice(bot_responses['sunny'])
     elif user == 'rainy':
-        return choice(bot_response_rainy)
+        return choice(bot_responses['rainy'])
     elif user == 'smokey':
-        return choice(bot_response_smokey)
+        return choice(bot_responses['smokey'])
     elif user == 'foggy':
-        return choice(bot_response_foggy)
+        return choice(bot_responses['foggy'])
     elif user == 'snowy':
-        return choice(bot_response_snowy)
+        return choice(bot_responses['snowy'])
     else:
         return('I dont have this weather in my database')
 
 
-print('Hello, Im Weather Bot')
+print(f'Hello, Im Weather Bot')
+print(f'Type "EXIT" to quit out')
 
 while True:
     user_input = input('Enter the weather: ')
